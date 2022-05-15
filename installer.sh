340af4676351e3fdd9f4e26290db8f8e72f1cf9c
rogerramosruiz <rogerramosruizdev@gmail.com>
2022-05-14 21:54:24 -0400

    uncomented no machien

--- installer.sh
+++ installer.sh
@@ -13,9 +13,9 @@ apt -y install ubuntu-mate-desktop
 
 # install nomachine for remote desktop
 
-# wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb
-# apt install ./nomachine_7.9.2_1_amd64.deb
-# rm nomachine_7.9.2_1_amd64.deb
+wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb
+apt install ./nomachine_7.9.2_1_amd64.deb
+rm nomachine_7.9.2_1_amd64.deb
 
 snap install blender --classic
 # sudo apt install firefox
