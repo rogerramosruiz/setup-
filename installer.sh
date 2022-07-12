ff8bee8fa6cfaeba3a0a443fea6ed1f0ea671891
rogerramosruiz <rogerramosruizdev@gmail.com>
2022-07-11 23:12:46 -0400

    linpode

--- installer.sh
+++ installer.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+if sudo -n true 2>/dev/null; then
+    echo ""
+else
+    echo "Wrong password"
+    exit 1
+fi
 export DEBIAN_FRONTEND=noninteractive
 
 apt update && apt upgrade -y
-apt install ubuntu-mate-desktop
+sudo apt -y install gnome-session gdm3
 
 # install nomachine for remote desktop
-apt install build-essential linux-headers-$(uname -r)
+sudo apt install build-essential linux-headers-$(uname -r)
 
 wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb
 apt install ./nomachine_7.9.2_1_amd64.deb
@@ -16,5 +23,6 @@ apt install ./google-chrome-stable_current_amd64.deb
 rm ./google-chrome-stable_current_amd64.deb
 apt install unzip
 apt -y install python3.10-venv
+apt install -y dolphin
 
-reboot
+sudo reboot
