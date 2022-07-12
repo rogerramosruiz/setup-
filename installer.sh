36155e28028a371820d0f50428a3fb168005a1b8
rogerramosruiz <rogerramosruizdev@gmail.com>
2022-07-11 22:02:57 -0400

    no sudo

--- installer.sh
+++ installer.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
-if sudo -n true 2>/dev/null; then
-    echo ""
-else
-    echo "Wrong password"
-    exit 1
-fi
 export DEBIAN_FRONTEND=noninteractive
 
 apt update && apt upgrade -y
-sudo apt install ubuntu-mate-desktop
+apt install ubuntu-mate-desktop
 
 # install nomachine for remote desktop
-sudo apt install build-essential linux-headers-$(uname -r)
+apt install build-essential linux-headers-$(uname -r)
 
 wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb
 apt install ./nomachine_7.9.2_1_amd64.deb
@@ -24,4 +17,4 @@ rm ./google-chrome-stable_current_amd64.deb
 apt install unzip
 apt -y install python3.10-venv
 
-sudo reboot
+reboot
